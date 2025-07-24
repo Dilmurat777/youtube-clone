579a8833aab164c302dd366c60f73ba5333fc651
user.email <mrdilmurat777@gmail.com>
2025-07-24 21:49:02 +0600

    feat: initial setup of the clone project youtube

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+import menu_icon from "../../assets/menu.png"
+
+const Navbar = () => {
+  return (
+	<nav className="flex-div">
+		  <div className="nav-left flex-div">
+			  <img src={menu_icon} alt="" />
+	  </div>
+	</nav>
+  )
+}
+
+export default Navbar
